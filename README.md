c2aeecae3157fcbc2c1868cb3d010e1e65790092
luceritopico2020@gmail.com <luceritopico2020@gmail.com>
2023-03-08 22:53:03 -0500

    se agregó primer_grado

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # primer_grado
 programa para resolver una ecuación de primer grado
+
+# Analisis
+
+# Diseño
+
+### Diagrama de flujo
+![Diagrama de flujo](diagrama.png"Diagrama de flujo")
---
+++ primer_grado.py
@@ -0,0 +1,10 @@
+
+print("hallar el valor de x en una ecuacion de primer grado")
+print("de la forma ax+b=0: ")
+
+a=int(input("ingrese el valor de a: "))
+b=int(input("ingrese el valor de b: "))
+
+x=b/a
+
+print("el valor de x es:"+str(x))
